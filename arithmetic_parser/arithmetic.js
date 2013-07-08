087bcca33543e9fa027dba944f05fd0581738d36
Daniel Na <daniel.na@gmail.com>
2013-07-08 18:00:52 -0400

    cleanup

--- arithmetic_parser/arithmetic.js
+++ arithmetic_parser/arithmetic.js
@@ -58,51 +58,6 @@
         return [arr2[0], arr2[1], calculatedValue + ""];
     };
 
-    // // Break large formula into an array of sub-formulas
-    // // [left, operator, right]
-    // Interpreter.prototype.parseTokens = function() {
-    //     var temp = [];
-    //     for (var i = 0; i < this.tokens.length; i++) {
-    //         var last = this.tokens[i];
-    //         temp.push(last);
-
-    //         // Currently based on index, since you know sub-formulas are 3 tokens long
-    //         if (i%2 === 0 && i !== 0) {
-    //             this.parsedTokens.push(temp);
-    //             temp = [];
-    //             temp.push(last);
-    //         }
-    //     }
-    //     console.log("this.parsedTokens:", this.parsedTokens);
-    // };
-
-    // Interpreter.prototype.calculate = function(){
-    //     if (this.parsedTokens.length > 1){
-    //         var x = this.combine(this.parsedTokens[0], this.parsedTokens[1]);
-    //         this.parsedTokens.splice(0, 1);
-    //         this.parsedTokens.splice(0, 1, x);
-    //         var cloned = this.parsedTokens.slice(0);
-    //         this.calculate();
-    //     }
-    // };
-
-    // Interpreter.prototype.combine = function(arr1, arr2){
-    //     console.log("arr1:", arr1);
-    //     console.log("arr2:", arr2);
-    //     var returnArray = [];
-    //     if (this.operators.indexOf(arr1[1]) < this.operators.indexOf(arr2[1])) {
-    //         returnArray = this.getCalculatedValue(arr1, arr2, true);
-    //     } else {
-    //         returnArray = this.getCalculatedValue(arr2, arr1);
-    //         // need to change that value here
-    //         console.log("here, returnArray:", returnArray);
-    //         var cloned = this.parsedTokens.slice(0);
-    //         console.log("this.parsedTokens:", cloned);
-    //     }
-    //     console.log("returnArray:", returnArray);
-    //     return returnArray;
-    // };
-
     Interpreter.prototype.go = function() {
         this.cleanString();
         return this.calculate();
