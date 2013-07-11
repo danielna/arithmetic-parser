042fdcafdb44aa3387715129c3c48b22186c5fb4
Daniel Na <daniel.na@gmail.com>
2013-07-11 18:07:06 -0400

    Some code cleanup

--- arithmetic_parser/arithmetic.js
+++ arithmetic_parser/arithmetic.js
@@ -13,7 +13,6 @@
         this.tokens = [];
         this.stack = [];
         this.outputQueue = [];
-        // this.operators = ["^", "*", "/", "+", "-"]; // ordered by preference
         this.operators = {
             "^": {
                 "precedence": 4,
@@ -68,22 +67,18 @@
             var c = parseInt(x, 10);
             return c ? c : x;
         });
-        // console.log(this.tokens);
         return this.tokens;
     };
 
     Interpreter.prototype.interpret = function(tokens) {
         for (var i = 0; i < tokens.length; i++) {
             var t = tokens[i];
-            // console.log("this token:", t);
             if (typeof t === 'number') {
                 this.outputQueue.push(t);
             } else if (t === "(") {
                 this.stack.push(t);
             } else if (t === ")") {
                 for(var k = this.stack.length-1; k >= 0; k--){
-                    // console.log("this.stack:", this.stack);
-                    // console.log("stack[k]:", this.stack);
                     if (this.stack[k] === "(") {
                         this.stack.pop();
                         break;
@@ -94,8 +89,6 @@
             } else if (this.operators[t]) {
                 var q = this.operators[this.stack[this.stack.length-1]];
                 var tObj = this.operators[t];
-                // console.log("q:", q);
-                // console.log("tObj:", tObj);
                 if (q && ((q.precedence > tObj.precedence) || ((q.precedence === tObj.precedence) && tObj.direction === "left"))) {
                     this.outputQueue.push(this.stack.pop());
                     this.stack.push(t);
@@ -103,21 +96,15 @@
                     this.stack.push(t);
                 }
             }
-            // console.log("outputQueue:", this.outputQueue);
-            // console.log("stack:", this.stack);
         }
 
         for (var j = 0; j <= this.stack.length; j++){
             this.outputQueue.push(this.stack.pop());
         }
-
-        console.log("outputQueue:", this.outputQueue);
-        console.log("stack:", this.stack);
     };
 
     Interpreter.prototype.calculate = function() {
-        var self = this,
-            output;
+        var self = this;
 
         var parseRPN = function(arr) {
             var arrTemp = [];
@@ -128,21 +115,16 @@
 
             for (var i = 0; i < arr.length; i++) {
                 var token = arr[i];
-
                 if (typeof token !== "number") {
 
                     var n2 = arrTemp.pop(),
                         n1 = arrTemp.pop();
 
-                    // console.log("n1:", n1);
-                    // console.log("n2:", n2);
-                    // // console.log("arrTemp2:", arrTemp);
-
                     answer = self.functions[token](n1, n2);
                     arrTemp.push(answer);
-                    // console.log("arrTemp3:", arrTemp);
                     arrTemp = arrTemp.concat(arr.slice(i+1));
                     break;
+
                 } else {
                     arrTemp.push(token);
                 }
@@ -161,6 +143,7 @@
     };
 
     exports.Interpreter = Interpreter;
+
 })(this);
 
 var pattern;
