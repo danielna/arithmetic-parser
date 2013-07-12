06c36d16a3acc9325e167b5c6b8c2bd0963cf512
Daniel Na <daniel.na@gmail.com>
2013-07-11 22:12:27 -0400

    Allow multi-digit values, like 123*45^(4-2)

--- arithmetic_parser/arithmetic.js
+++ arithmetic_parser/arithmetic.js
@@ -63,7 +63,22 @@
     Interpreter.prototype.cleanString = function() {
         this.tokens = this.str.split("").filter(function(x) {
             return x.trim();
-        }).map(function(x){
+        });
+
+        // Collapse sequences of digits into one digit
+        // i.e. 1,1,1 becomes 111
+        for (var i = this.tokens.length-1; i >= 0; i--){
+            if (i >= 1) {
+                var t = parseInt(this.tokens[i], 10) ? parseInt(this.tokens[i], 10) : "",
+                    t1 = parseInt(this.tokens[i-1], 10) ? parseInt(this.tokens[i-1], 10) : "";
+                if (typeof t == "number" && typeof t1 == "number") {
+                    this.tokens[i-1] =  t1.toString() + t.toString();
+                    this.tokens.splice(i, 1);
+                }
+            }
+        }
+
+        this.tokens = this.tokens.map(function(x){
             var c = parseInt(x, 10);
             return c ? c : x;
         });
@@ -101,6 +116,9 @@
         for (var j = 0; j <= this.stack.length; j++){
             this.outputQueue.push(this.stack.pop());
         }
+
+        console.log("this.outputQueue:", this.outputQueue);
+
         document.getElementById("RPN").innerHTML = this.outputQueue.join(" ").toString();
     };
 
